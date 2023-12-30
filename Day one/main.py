6c88cdceda63d05469ce2d91f0e7b5dd81aa0cf8
Temitope Ajanaku <temitope.ajanaku@inheaden.io>
2023-12-30 10:10:37 +0100

    Day 1 lessons

---
+++ Day one/main.py
@@ -0,0 +1,12 @@
+print("All good")
+print('print("What to print")')
+
+# String Manipulation
+# Using same print statement for multiple lines of code
+print("hello world\n new world \n another word")
+print("Temi" + " Tope")
+
+# Input Function
+# input("What is your name?")
+# Combining print and input function
+print("Welcome " + input("What is your name?"))
