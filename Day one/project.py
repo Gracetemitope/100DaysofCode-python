fc7d55c34b806123a55b9b57d6ac36447da4cf0a
Temitope Ajanaku <temitope.ajanaku@inheaden.io>
2023-12-30 10:11:34 +0100

    band generator

---
+++ Day one/project.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name generator")
+city_name = input("What is the name of the city you grew up in?\n")
+pet_name = input("What is the name of your favorite pet?\n")
+print("Your band name could be " +  city_name + " " + pet_name)
+ 
